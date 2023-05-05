c047480cdf185e2d7b58e17489f911b167d92d4d
wjddns0122 <jwoon2004@gmail.com>
2023-05-05 22:18:09 +0900

    2complete

--- lib/main.dart
+++ lib/main.dart
@@ -13,6 +13,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         backgroundColor: const Color(0xFF181818),
         body: SingleChildScrollView(
